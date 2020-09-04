26824b925a18b36f8dda237695e28162ea62b3f8
vincemis610 <vincemis610@gmail.com>
2020-09-04 02:34:07 -0500

    Uploading First Build App

---
+++ docs/precache-manifest.f67fdfe24727fa68eca4894efb798ad6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ec7837c2b78caf8f1a8846363657e0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6972019b9756b76bfb38",
+    "url": "/static/js/2.fd247436.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fd247436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "456e6001bcc4f45fc2fe",
+    "url": "/static/js/main.073eb592.chunk.js"
+  },
+  {
+    "revision": "669a532d7cd0b3017d4e",
+    "url": "/static/js/runtime-main.93d6047e.js"
+  }
+]);
