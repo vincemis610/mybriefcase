edb16f6fb681db7c8afd894e495a635241799d9e
vincemis610 <vincemis610@gmail.com>
2020-09-06 15:15:46 -0500

    Images screen

--- src/data/projects.js
+++ src/data/projects.js
@@ -3,7 +3,7 @@ export const projects = [
         'id':'1',
         'project': 'Town Hall Web Site',
         'technology': 'PHP, MySQL, Bootstrap, Javascript',
-        'screen': 'https://lh3.googleusercontent.com/LA4kWqubECARqy6INmMTZUcFWF-l9sbXfhXj0UBOGTpuZyBIV7LZk--jmx7ZB4kZ74NTiotMmnL21AmmK-xWUuXE4X4u_5HfNu8S7hjhkOwuE34W3xqwGLdi8Gnub0jAadhj-wpmUkySsp4GDvGXJGTDC9BQnlgUAKB246qDxrBwr6mOeqKS47lnT1Q-RYLritQAKIZe3abvWRRINNmmTAe5_eAyAIGeOnCyW000CnBZeFhGgsDiLV9HGvyLbqvMoXzkzDZwETTeAl-QNj_hNVwBpsGDQROPxCJbsgoSIbh6VEyyX-9afjYgUjvw3r5leADb-Emr44CIqi6T4ncLY5zORZMKrD-9UlPNldSX0jJS1OVQvhMNj9R4c9TwfGl8aNTlL5VRzCMVIzkxiuCeY3SPofsJDcJIGv1E47v5mHxT7JjknbayXtqEvoYp61f5CFyNqrlfMbFVBJO_FWq4uXcoA7TfjojggURKe0t0BFxZ7PApdlWJyqCXcRWHYt4tN7LLdq87mObHwTEp_83DnOkXY6_hSQqJvkY9TefoWg1_rZAewAs2bsPwpBrVFznM_1I7wHlAErjONRiVTXX4OJ-RBrVqMcOKTzRnp-7u7biM_ACg-5Fya98zwjbcncKw7dGOp4VHo8tiaW1a3vYVpthSewOKIkT9ruaK_XgD1a0D7ackxda0fR4GMWmBeg=w366-h220-no?authuser=0',
+        'screen': 'teabo.jpeg',
         'link': 'https://www.teaboyucatan.gob.mx/',
         'description': 'Is a informative website with gallery and documents in a DB'     
     },
@@ -11,7 +11,7 @@ export const projects = [
         'id':'2',
         'project': 'Selftour WebApp',
         'technology': 'NodeJs, Firebase AWS, Javascript',
-        'screen': 'https://lh3.googleusercontent.com/rT2K5MsUwiSK7vyx6Xbx_LFtbLPOb0U2renwvapVlkquLySDLIUa1RPJ0SKfw4WXhEOBICwML9G6mvD0csVStNhigKoJVa_qqG8EZjtMjIq7aVqKqiR4lX_HdzOlryLhIrQBYvh27ivFJnbVsivqYphrTdgGaLIa2W9jmm5CnlQr9uOD--dLSfnlvHysSIUEKMBJuAxjL4iwvyfxAAmDxBP0GgDsn9hKtJzcWiURL9M1mskCDEKwQjBFmHlDbWyos9DXRbcg7QpzgsgY78lTKsFa5nZVuowLvk5a_LbvA3ahYOYwSyNuLy0hEZNidSGkd4glaUY47iJojJxKndKcinJe2Nd9ENvxaIJLflFexlxvhgoHvds41MwyA9GTYB4cFfn8b6aX-pLrsBPZGRbETYQOy0MJaUFB0EhGOLfXY8nuNly7N_SA2q5jgoY_lOzEyc5YYH4PO20s97qaQKM7Qw5RBLJy2ZqynukA2ZTbybQC5z_TdKvchSSEWYCxlwMhPjWP7yqgowpCMZgtaE0dMi4CFoNqbvbqb_Ln8ZmqFM8jryaTsjVSn8Vu_94A5TKlc2RONLob6Hy8AFkgq89WLS_QvyPyu5QZkDNKfUXuqYvPYYteotxDV_A7ceKEB9yBdw5WQg10M5tIwzsuFOnvyuEgyN1q0QU__upMdgMNseKSJLuvaG4rNQGrMi31ag=w1212-h670-no?authuser=0',
+        'screen': 'selftour.jpeg',
         'link': 'https://www.selftour.travel',
         'description': 'Is a platform where peoples share tours and experiences, they could select places in google map and trace route, add pictures and sell or buy tours'     
     },
@@ -19,7 +19,7 @@ export const projects = [
         'id':'3',
         'project': 'Netclone',
         'technology': 'React JS, Firebase',
-        'screen': 'https://lh3.googleusercontent.com/Zd05lTJmBgQxK0ObRj5ojqZfDYZ1gnCHb-TkKneK2nTzh1KTP9pG4_usH-JOZbN_9ElNgxlRLgOQIqFPMpsO-Mfm9vS7Nhnc_tPwNeURqxclulqw6-aa8NWv-n3bMAPjXCSBO5Zg3Ab0jN9hvvTNUn7Z0tpfaaM3kcP5kXP5STme8bTbQgarTFNZEg9UswvDnmlJtAnJ_3FpzZl7hyrAWCLex1YK24c5f2ndqPQi4F6V4reVABSR_-IQZfwTdQUTLjmvbV62wmR9FmMvCuyFyoWYcyfz4luIWO_eE1SEX1YDf8r0sj1TxCqg-DG8s5u5I9v--3v5Nb0zEia8x6KoAf_vRYqT8Lbm0L_gKMUoGpaxJ8LBYUKVYEuXoYgpyEMHiYzrzO-_UsOvFeMBqPI1IlcggwHo_kSnRikjv-xLefbUqjainMDUIAy6y8OMPOLMpwbly3qC6cJwCGUhaUPgA-tXWgyqTpWD9rEbTs3QmiTvyZBwDehTHOnGQUeVsXYrvgPBYsGoRJ0lKLAR6uS4P9tl72mV3awN3CqcFNJqSdzuXo0HXJb0PIXr5Z8eowDLC8gvgwk8Qx0pIA4dKTll2NO15R9h4CkfdMuOvl_TlyIbNNXx7vxwYoMCZd4o0hsvXtPscfkzDuev9EHo7T_K77YkGW3Ut6JpDswKkGgV3jdaOzNGgoGJiTWp6k5wVA=w357-h185-no?authuser=0',
+        'screen': 'netclone.jpeg',
         'link': 'https://netclone-app.web.app/',
         'description': 'Is a website a little clone of Netflix, its for educational purposes only with React JS'
     }
