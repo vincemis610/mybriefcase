68c651aceb374ae6d1c1688273f4678883338464
vincemis610 <vincemis610@gmail.com>
2020-09-06 15:00:32 -0500

    new project

---
+++ docs/precache-manifest.20037ba165da0e45c59d82ad9f87dcc2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c3ce80b739d3dd2fb78bc9fb7b03ca6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6972019b9756b76bfb38",
+    "url": "/static/js/2.fd247436.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.fd247436.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a93f1caf04af1b50029",
+    "url": "/static/js/main.ec8bac38.chunk.js"
+  },
+  {
+    "revision": "669a532d7cd0b3017d4e",
+    "url": "/static/js/runtime-main.93d6047e.js"
+  }
+]);
